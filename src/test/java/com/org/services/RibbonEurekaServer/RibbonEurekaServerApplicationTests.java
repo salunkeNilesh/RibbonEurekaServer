e5497c8aad4d83e806445b8547083a03609f00b1
unknown <nilesh.salunke@briozing.com>
2020-09-09 18:10:38 +0530

    initial commit

---
+++ src/test/java/com/org/services/RibbonEurekaServer/RibbonEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.services.RibbonEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibbonEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
